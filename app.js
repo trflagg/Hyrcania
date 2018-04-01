1d1094e8e3e0e69bc8a0cfe1602e9dd2109b590d
Taylor Flagg <trflagg@gmail.com>
2018-04-01 13:34:12 -0400

    Add /new and /messages api
    I couldn't get the {name: 1} projection to work on the /messages
    Don't know why but I should go back and figure it out

--- app.js
+++ app.js
@@ -1,9 +1,46 @@
 const express = require('express');
 const app = express();
 
+const Db = require('argieDB/db');
+
+
+// Connect to DB
+console.log('Connecting to DB');
+environment = require('./db-environment-default.js');
+if (process.env.DB_ENV === 'compose') {
+    environment = require('./db-environment-compose.js');
+}
+// hide the username:password in the URL string
+console.log('db URL: '+environment.db.URL.replace(/:\/\/.*:(.*)@/, 'XXXXXXX'));
+const db = new Db(environment);
+db.connect();
+console.log('Connected.');
+
+
+// load models
+const Message = require('argie/models/message')(db);
+
+// routes
 app.get('/', (req, res) => res.send('Hyrcania!'));
 
 app.use('/static', express.static('client/build/static'));
+
+app.get('/new', (req, res) => {
+  let newMessage = db.create('Message');
+
+  newMessage.setName('TEST1');
+  newMessage.setText('Hello world');
+
+  db.save('Message', newMessage).then(() => {
+    res.send('New Message created');
+  });
+});
+
+app.get('/messages', (req, res) => {
+  db.loadMultiple('Message', {}).then(results => {
+    res.send(results);
+  }).catch(e => { res.send(e); });
+});
 
 let port = 3000;
 if (process.env.NODE_ENV === 'production') {
--- makefile
+++ makefile
@@ -1,6 +1,9 @@
 build:
 	docker build . -t hyrcania
 
+mongo-compose:
+		docker run -it --link hyrcania_mongo_1:mongo --rm mongo:3.0.5 sh -c 'exec mongo "$$MONGO_PORT_27017_TCP_ADDR:$$MONGO_PORT_27017_TCP_PORT"'
+
 run-prod:
 	docker run -p 80:80 hyrcania
 
