42b5d19bc1f9d333643dc8aaf067648fa11aefbd
Taylor Flagg <trflagg@gmail.com>
2018-04-01 12:39:47 -0400

    Connect to mongodb on app startup

---
+++ db-environment-compose.js
@@ -0,0 +1,4 @@
+module.exports.db = {
+  // build the mongo URL from the env vars exposed by docker
+  URL: 'mongodb://' + process.env.MONGO_PORT_27017_TCP_ADDR + ':' + process.env.MONGO_PORT_27017_TCP_PORT
+}
